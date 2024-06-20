f67a5fab30aa031e54bca16ddbd8fdc134511330
S1U <4564129+S1U@users.noreply.github.com>
2024-06-20 08:36:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,6 @@
 
 # Add a feed source
 echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
