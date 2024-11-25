ee0769cd55c6b3ab239a1a2700050d4b77515ecd
S1U <4564129+S1U@users.noreply.github.com>
2024-11-25 21:29:34 +0800

    Update custom-wan.sh

--- custom-wan.sh
+++ custom-wan.sh
@@ -10,5 +10,6 @@ set network.wan.ipaddr=192.168.1.11
 set network.wan.netmask=255.255.255.0
 set network.wan.gateway=192.168.1.1
 add_list network.wan.dns=223.5.5.5
-commit network
 EOF
+
+commit network
